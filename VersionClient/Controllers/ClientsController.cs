4562508054e50149b5a4f786c2a13fc8d92c6c8d
PauloSanRibeiro <paulo_pr11@hotmail.com>
2021-09-03 15:24:03 -0300

    Criado Views Details e Edit

--- VersionClient/Controllers/ClientsController.cs
+++ VersionClient/Controllers/ClientsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using VersionClient.Models;
 
@@ -23,10 +25,11 @@ namespace VersionClient.Controllers
         //GET: Clients/Create
         public IActionResult Create()
         {
-            
+
             return View();
         }
 
+        //POST: Clients/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("IdClient,NameClient,UrlLogin")] Client client)
@@ -38,6 +41,77 @@ namespace VersionClient.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(client);
+        }
+
+        //GET: Clients/Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Client.FirstOrDefaultAsync(m => m.IdClient == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);
+        }
+
+        //GET: Clients/Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Client.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            return View(client);
+        }
+
+        //POST: Clients/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdClient,NameClient,UrlLogin")] Client client)
+        {
+            if (id != client.IdClient)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(client);
+                    await _context.SaveChangesAsync();
+                }
+                catch (ApplicationException)
+                {
+                    if (!ClientExists(client.IdClient))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(client);
+        }
+
+        private bool ClientExists(int id)
+        {
+            return _context.Client.Any(e => e.IdClient == id);
         }
 
 
